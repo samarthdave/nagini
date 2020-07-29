efe493d9d10de2e24f5df1d8f71ef8fbebd01137
Samarth Dave <samarthdave@users.noreply.github.com>
2020-07-29 18:12:07 -0500

    Finish readability

--- cs50x/ps6/readability.py
+++ cs50x/ps6/readability.py
@@ -1,35 +1,45 @@
 # ps6 - cs50x
-import re # use regex
 
 def main():
     print("#### Readability ####")
 
-    # user_input = input("Text: ")
-    user_input = "Congratulations! Today is your day. You're off to Great Places! You're off and away!"
+    user_input = input("Text: ")
 
     result = get_score(user_input)
     print(f"Grade {result}")
 
 # use Coleman-Liau formula
 # 0.0588 * L - 0.296 * S - 15.8
-# L - avg # of letters per 100 words
-# S is the average number of sentences per 100 words
 def get_score(_str_input):
     _clean = _str_input.strip()
 
-    _split_words = re.split(r'[ .!?\']+', _clean)
-    total_sentence_count = len(list(filter(lambda x: len(x) > 0, re.split(r'[.!?]+', _clean))))
-    total_letter_count = sum([len(word) for word in _split_words])
+    # all integers for calculations
+    i = 0
+    digit_count = 0
+    letter_count = 0
+    word_count = 1 # start at 1 since we need to count the first occurrence
+    sentence_count = 0
 
-    sum_until_100 = sum([len(word) for word in _split_words[:100]])
-    L = sum_until_100 / min(len(_split_words), 100)
+    for letter in _clean:
+        if letter.isalpha():
+            letter_count += 1
+        elif letter.isnumeric():
+            digit_count += 1
+        elif letter == ' ' and _clean[i + 1]:
+            word_count += 1
+        elif (letter == "!" or letter == "." or letter == "?"):
+            sentence_count += 1
+        # increment total length
+        i += 1
 
-    print(f"Total letters: {total_letter_count}")
-    print(f"Sentence count: {total_sentence_count}")
-    print(f"L value: {L}")
+    # L - avg # of letters per 100 words
+    L = float(letter_count * 100 / word_count)
+    # S is the average number of sentences per 100 words
+    S = float(sentence_count * 100 / word_count)
 
-    # result = 0.0588 * L - 0.296 * S - 15.8
-    return 1
+    # calculate & round answer
+    result = 0.0588 * L - 0.296 * S - 15.8
+    return int(round(result, 0))
 
 if __name__ == "__main__":
     main()
