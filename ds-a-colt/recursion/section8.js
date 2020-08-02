26beccf511a556c7c2f3a297569a9740895ec145
Samarth Dave <samarthdave@users.noreply.github.com>
2020-08-01 19:43:57 -0500

    Implement iterative fib

--- ds-a-colt/recursion/section8.js
+++ ds-a-colt/recursion/section8.js
@@ -52,3 +52,18 @@ function fib(num) {
 
     return fib(num - 2) + fib(num - 1);
 }
+
+function fibIterative(n) {
+    if (n <= 1) return n;
+
+    let fib0 = 0;
+    let fib1 = 1;
+    let sum = 0;
+    for (let i = 2; i <= n; i++) {
+        sum = fib0 + fib1;
+        fib0 = fib1;
+        fib1 = sum;
+    }
+
+    return sum;
+}
