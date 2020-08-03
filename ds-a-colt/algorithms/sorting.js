3ffca4a2576285d200d5ad08f49b135fcd360981
Samarth Dave <samarthdave@users.noreply.github.com>
2020-08-03 16:57:20 -0500

    Cleanup code for es6 syntax

--- ds-a-colt/algorithms/sorting.js
+++ ds-a-colt/algorithms/sorting.js
@@ -2,18 +2,17 @@
 // inefficient but I also forgot how to implement this so here goes
 
 function bubbleSort(arr) {
+    const swap = (target, index1, index2) => {
+        // es2015 swap
+        [target[index1], target[index2]] = [target[index2], target[index1]]
+    }
     for (let i = arr.length - 1; i >= 0; i--) {
         for (let j = 0; j < i; j++) {
-            console.log(arr, arr[j], arr[j+1])
             if (arr[j] > arr[j+1]) {
-                let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                swap(arr, j, j+1)
             }
         }
     }
-    console.log("done:")
-    console.log(arr);
     return arr;
 }
 
